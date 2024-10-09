afa000f38ccafcda3a5c425a255b876a155b81bb
Roland Espeleta <rolandvictorespeleta@gmail.com>
2024-10-09 15:12:26 +0800

    edit random guessing game error

--- random_guessing.py
+++ random_guessing.py
@@ -1,48 +1,51 @@
 import random
 
 
-def exit_game():
-  
-  play_again = input("Play again? y/n: ").lower()
-  if not play_again == "y":
-    return False
-  else:
-    return True
-  
 def number_guessing():
-  running = True
-  lowest_num = 1
-  highest_num = 1000
-  answer = random.randint(lowest_num, highest_num)
-  guesses = 0
+  
+  playing = True
+  
+  while playing:
+    
+    running = True
+    lowest_num = 1
+    highest_num = 1000
+    answer = random.randint(lowest_num, highest_num)
+    guesses = 0
 
-  while running:
-      
-    not_guessed = True
-    print("Python Number Guessing Game")
-    print("Select number between 1 to 1000")
+    while running:
+        
+      not_guessed = True
+      print("Python Number Guessing Game")
+      print("Select number between 1 to 1000")
 
-    while not_guessed:
-      
-      guess = input("Enter your guess: ")
+      while not_guessed:
+        
+        guess = input("Enter your guess: ")
 
-      if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
-        if guess < lowest_num or guess > highest_num:
-          print("Guess out of range")
-          print("Please select number between 1 to 100")
-        elif guess < answer:
-          print("Too low!")
-        elif guess > answer:
-          print("Too high")
+        if guess.isdigit():
+          guess = int(guess)
+          guesses += 1
+          if guess < lowest_num or guess > highest_num:
+            print("Guess out of range")
+            print("Please select number between 1 to 100")
+          elif guess < answer:
+            print("Too low!")
+          elif guess > answer:
+            print("Too high")
+          else:
+            print(f"Correct! The answer was: {guess}")
+            print(f"The number of guesses: {guesses}")
+            play_again = input("Play again? y/n: ").lower()
+            if not play_again == "y":
+              playing = False
+            else:
+              playing = True
+              not_guessed = False
+              running = False
         else:
-          print(f"Correct! The answer was: {guess}")
-          print(f"The number of guesses: {guesses}")
-          running = exit_game()
-      else:
-        print("Invalid guess")
-        print("Please select number between 1 to 1000")
+          print("Invalid guess")
+          print("Please select number between 1 to 1000")
             
     
 if __name__ == '__main__':
--- rps.py
+++ rps.py
@@ -7,13 +7,10 @@ def win_condition(player, computer):
       print("It's a tie! ")
       return "Game Draw"
     elif player == "🪨" and computer == "✂️":
-      print("Player win!")
       return "Player Win"
     elif player == "📃" and computer == "🪨":
-      print("Player win!")
       return "Player Win"
     elif player == "✂️" and computer == "📃":
-      print("Player wins!")
       return "Player Win"
     else:
       print("Computer wins!")
@@ -31,46 +28,57 @@ def rps_game():
   player_score = 0
   computer_score = 0
   draws = 0
-
-  while running:
-
-    player = None
-    computer = random.choice(options)
+  game_on = True
+  
+  while game_on:
     
-    while player not in options:
+    race_to = input("RPS Game Race to (enter a number)?: ")
+    
+    if race_to.isdigit():
+      race_to = int(race_to)
+      print()
+      print(f"RPS Game Race to {race_to}")
       
-      player = input("Enter rock or paper or scissors: ")
-      if player == "rock":
-        player = "🪨"
-      elif player == "paper":
-        player = "📃"
-      elif player == "scissor":
-        player = "✂️"
-      else: 
-        print("Only rock, paper, scissors")
-    print(f"Player: {player}")
-    print(f"Computer: {computer}")
+      while running:
+
+        player = None
+        computer = random.choice(options)
+        
+        while player not in options:
           
-    score = win_condition(player, computer)
-    if score == "Player Win":
-      player_score += 1
-    elif score == "Computer Win":
-      computer_score += 1
-    elif score == "Game Draw":
-      draws += 1
-
-    playing_again = True
-    
-    while playing_again:
+          player = input("Enter rock or paper or scissors: ")
+          if player == "rock":
+            player = "🪨"
+          elif player == "paper":
+            player = "📃"
+          elif player == "scissor":
+            player = "✂️"
+          else: 
+            print("Only rock, paper, scissors")
+        print(f"Player: {player}")
+        print(f"Computer: {computer}")
+              
+        score = win_condition(player, computer)
+        if score == "Player Win":
+          player_score += 1
+          print(f"Player {player_score} : {computer_score} Computer")
+          if player_score == race_to:
+            print("Player Wins!!!")
+            game_on = False
+            break
+        elif score == "Computer Win":
+          computer_score += 1
+          print(f"Player {player_score} : {computer_score} Computer")
+          if computer_score == race_to:
+            print("Computer Wins!!!")
+            game_on = False
+            break
+        elif score == "Game Draw":
+          draws += 1
+    else:
+      print("That is not a number")
       
-      play_again = input("Enter to play again (s: see score q: quit): ").lower() 
-      if play_again == "q":
-        running = False
-        playing_again = False
-      elif play_again == "s":
-        show_score(computer_score, player_score, draws)
-      else:
-        playing_again = False
-
+  
+  
 if __name__ == '__main__':
   rps_game()
